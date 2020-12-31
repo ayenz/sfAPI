d3e14366af7664027c65309df8a9cf1df0d2a489
ayenz <stillmen.v@gmail.com>
2020-12-31 23:49:04 +0700

    Handling 404 not found

--- app.js
+++ app.js
@@ -23,5 +23,10 @@ app.get("/", (req, res) => {
     res.send("We are on HOme, Hello world bethc!");
 });
 
+
+app.use((req, res, next) => {
+    res.status(404).send("404 Nothing to do here")
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
